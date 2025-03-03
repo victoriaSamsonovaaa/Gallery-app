b0665fc14b7cd48a2cf312c038d5a60d357f219e
VIKTORYIA SAMSONAVA <viktoriya09june@icloud.com>
2025-03-03 14:59:02 +0300

    created searching VC structure

--- Gallery app/Gallery app/PhotosSearchingViewController.swift
+++ Gallery app/Gallery app/PhotosSearchingViewController.swift
@@ -11,6 +11,37 @@ import UIKit
 class PhotosSearchingViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .yellow
+        creatingNavigationBar()
+        creatingSearchBar()
+        creatingCollectionView()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellId", for: indexPath)
+        cell.backgroundColor = .cyan
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    private func creatingNavigationBar() {
+        let appTitle = UILabel()
+        appTitle.text = "Gallery App"
+        appTitle.font = UIFont.systemFont(ofSize: 20, weight: .light)
+        appTitle.textColor = .black
+        navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: appTitle)
+    }
+    
+    private func creatingSearchBar() {
+        let searchField = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchField
+        searchField.hidesNavigationBarDuringPresentation = false
+        searchField.obscuresBackgroundDuringPresentation = false
+    }
+    
+    private func creatingCollectionView() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CellId")
     }
 }
--- Gallery app/Gallery app/PrimaryTabBarController.swift
+++ Gallery app/Gallery app/PrimaryTabBarController.swift
@@ -25,3 +25,5 @@ class PrimaryTabBarController: UITabBarController {
         viewControllers = [galleryNavigationVC, favouriteNavigationVC]
     }
 }
+
+
